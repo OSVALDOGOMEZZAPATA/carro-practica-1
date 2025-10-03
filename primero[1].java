dfe7879d328686067a6f5d1e18f2624cfb0f2dbd
OSVALDOGOMEZZAPATA <gomezosvaldo662@gmail.com>
2025-10-02 22:41:22 -0600

    Add files via upload

---
+++ primero[1].java
@@ -0,0 +1,23 @@
+package practica1
+
+public class primero{
+    public static void main (String [] args){
+        String saludo = "Hola mundo";
+        int edad = 20;
+        float altura = 1.75f;
+        System.out.println(Saludo);
+        System.out.println("Edad: " + edad);
+        system.out.println("Altura: " + altura);
+
+
+
+        Datos persona = new datos ("Juan", 25, "juan@hotmail.com");
+
+        System.out.println("Nombre:" + persona.getnombre();)
+        System.out.println("Edad:" + persona.getnombre());
+        System.out.println("Correo:" + persona.getnombre());
+        
+    }
+
+
+}
